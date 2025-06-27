4941b777ed34123aa646d3d01c007e08b7f3996d
Mohit Meemrauth <mohi1038@Mac.lan>
2025-06-28 04:08:31 +0530

    Add TypeScript type definitions for Creator

---
+++ frontend/types/Creator.ts
@@ -0,0 +1,13 @@
+export interface Creator {
+  id?: number;
+  name: string;
+  image_url?: string;
+  genre?: string;
+  description?: string;
+  audio_preview?: string;
+  dnaVisibility: string;
+  price: string;
+  license: string;
+  tracks: string;
+  partner: string;
+}
