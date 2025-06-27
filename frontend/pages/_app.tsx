92e20c636421fe56f108ef0fe1711f887f6fdb6a
Mohit Meemrauth <mohi1038@Mac.lan>
2025-06-28 04:11:20 +0530

    Add Next.js app wrapper component

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#18191B" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp; 
