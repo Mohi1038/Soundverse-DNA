f8f4e65750715e1f38bfe6483eda07aec1c20ea5
Mohit Meemrauth <mohi1038@Mac.lan>
2025-06-28 04:11:34 +0530

    Add profile menu component

---
+++ frontend/components/ProfileMenu.tsx
@@ -0,0 +1,9 @@
+export default function ProfileMenu() {
+  return (
+    <div className="absolute right-0 mt-2 w-40 bg-[#232428] rounded shadow-lg py-2 z-20">
+      <a href="#" className="block px-4 py-2 text-sm text-white hover:bg-[#18191B]">Profile</a>
+      <a href="#" className="block px-4 py-2 text-sm text-white hover:bg-[#18191B]">Settings</a>
+      <a href="#" className="block px-4 py-2 text-sm text-white hover:bg-[#18191B]">Logout</a>
+    </div>
+  );
+} 
